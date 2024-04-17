2e74da2414a939817b164fd18dc2373b2cf05a10
emaminotti <emanuele.minotti@mail.polimi.it>
2024-04-17 16:06:34 +0200

    lidar_remap.cpp update

--- first_project/src/lidar_remap.cpp
+++ first_project/src/lidar_remap.cpp
@@ -1,18 +1,15 @@
-
 #include "ros/ros.h"
 #include "nav_msgs/Odometry.h"
 #include "dynamic_reconfigure/server.h"
-#include "first_project/lidarConfig.h" // Importa il file di configurazione dinamica generato
-
-#include "std_msgs/String.h"
+#include "first_project/lidarConfig.h" // Import the generated dynamic configuration file
 
 class LidarRemapNode {
 private:
-    std::string frame; //frame a cui devono essere rimappati i messaggi
+    static std::string frame; // Frame to which messages should be remapped
     ros::NodeHandle nh;
     ros::Subscriber odom_sub;
     ros::Publisher remapped_pub;
-    dynamic_reconfigure::Server<frame::RemapConfig> dyn_reconf_server;
+    dynamic_reconfigure::Server<first_project::lidarConfig> dyn_reconf_server;
     ros::Timer timer;
 
 public:
@@ -20,35 +17,35 @@ public:
         odom_sub = nh.subscribe("/os_cloud_node/points", 1, &LidarRemapNode::odomCallback, this);
         remapped_pub = nh.advertise<nav_msgs::Odometry>("/pointcloud_remapped", 1);
         dyn_reconf_server.setCallback(boost::bind(&LidarRemapNode::reconfigureCallback, this, _1, _2));
-        frame = "wheel_odom"; // Imposta il frame predefinito
-        timer = nh.createTimer(ros::Duration(1.0), &LidarRemapNode::timerCallback, this); // Timer a 1 Hz
+        frame = "wheel_odom"; // Set the default frame
+        timer = nh.createTimer(ros::Duration(1.0), &LidarRemapNode::timerCallback, this); // 1 Hz timer
     }
-/*
-Questo metodo viene chiamato ogni volta che il nodo riceve un messaggio sul topic /os_cloud_node/points.
-Rimappa il frame del messaggio in base al valore attuale di frame e lo pubblica sul topic /pointcloud_remapped.
-*/
+
+    // This method is called whenever the node receives a message on the /os_cloud_node/points topic.
+    // It remaps the frame of the message based on the current value of frame and publishes it on the /pointcloud_remapped topic.
     void odomCallback(const nav_msgs::OdometryConstPtr& msg) {
         nav_msgs::Odometry remapped_msg = *msg;
         remapped_msg.header.frame_id = frame;
         remapped_pub.publish(remapped_msg);
     }
 
-/*
-Questo metodo viene chiamato ogni volta che viene modificato un parametro nel server di configurazione
-dinamica. Aggiorna il valore di frame in base alla nuova configurazione.
-*/
-    void reconfigureCallback(frame::RemapConfig &config, uint32_t level) {
+    // This method is called whenever a parameter is modified in the dynamic reconfigure server.
+    // It updates the value of frame based on the new configuration.
+    void reconfigureCallback(first_project::lidarConfig &config, uint32_t level) {
         frame = config.frame;
     }
 
-/*
-Questo metodo viene chiamato a intervalli regolari dal timer. Pubblica il valore corrente di
-frame sul topic /pointcloud_remapped.
-*/
+    // This method is called at regular intervals by the timer.
+    // It publishes the current value of frame on the /pointcloud_remapped topic.
     void timerCallback(const ros::TimerEvent& event) {
-        remapped_pub.publish(frame);
+        nav_msgs::Odometry frame_msg;
+        frame_msg.header.frame_id = frame;
+        remapped_pub.publish(frame_msg);
     }
 };
+
+// Definition of the static member variable
+std::string LidarRemapNode::frame;
 
 int main(int argc, char **argv) {
     ros::init(argc, argv, "lidar_remap");
@@ -56,4 +53,3 @@ int main(int argc, char **argv) {
     ros::spin();
     return 0;
 }
-
